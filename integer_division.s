dc9e17fdad56eda3c7eb2c36d2644ce973b0c5d5
Andrea Cipriani <63643557+cipryyyy@users.noreply.github.com>
2024-06-29 00:22:03 +0200

    Improved integer_division.s

--- integer_division.s
+++ integer_division.s
@@ -1,26 +1,34 @@
 .data
-	dd: .word 12
-	dr: .word 3
+	dvd: .word -15	@dividend
+	dvs: .word -7	@divisor
 .bss
-	qz: .space 4
-	rm: .space 4
+	qt: .skip 4		@quotient
+	rm: .skip		@remainder
 .text
-	.global divide
-divide:
-	ldr r0,	=dd
+	mov r10, #0		@Works as a boolean, check if only one value is negative
+	ldr r0, =dvd	@Load values
 	ldr r0, [r0]
-	ldr r1,	=dr
+	ldr r1, =dvs
 	ldr r1, [r1]
-	mov r2, #0
-	mov r3, #-1
+	cmp r0, #0			@If it's negative
+	rsbmi r0, r0, #0	@Sign is flipped
+	addmi r10, #1		@Update the boolean
+	cmp r1, #0			
+	rsbmi r1, r1, #0
+	addmi r10, #1	
+	mov r2, #0		@Counter for the quotient
 _loop:
-	add r2, r1
-	add r3, #1
-	cmp r2, r0
-	blt	_loop
-	ldr r4, =qz
-	str r3, [r4]
-	subs r3, r0, r2
-	addmi r3, r1
-	ldr r4, =rm
-	str r3, [r4]
+	cmp r1, r0		@Divisor greater than dividend
+	bgt _save		@Yes, save the result
+	subs r0, r1		@No, decrement the divisor
+	add r2, #1		@Increase the quotient
+	b _loop			@Loop
+_save:
+	tst R10, #1 	@Check Z-Flag for parity
+	rsbne r0, r0, #0	@Bool is true, one value is negative
+	rsbne r2, r2, #0	@Flip the values
+	ldr r1, =qt			@Load the addresses and save
+	str r2, [r1]
+	ldr r1, =rm
+	str r0, [r1]
+	b _end 			@exit
